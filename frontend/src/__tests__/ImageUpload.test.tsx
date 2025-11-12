34bf244b1ef402643afc36ddb78618f5ac726f97
jayraj <jayraj@secretmindtech.com>
2025-11-12 16:01:07 +0530

    remove unused userEvent import

--- frontend/src/__tests__/ImageUpload.test.tsx
+++ frontend/src/__tests__/ImageUpload.test.tsx
@@ -1,6 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import ImageUpload from '../components/ImageUpload';
 import * as imageResize from '../utils/imageResize';
 
